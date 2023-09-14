8679184d339fd659a44b43eb122a7817daf322bb
merin-mary-shaji <142540160+merin-mary-shaji@users.noreply.github.com>
2023-09-13 21:41:44 -0500

    Update SignInPage.java

--- src/main/java/com/dsalgoportal/pages/SignInPage.java
+++ src/main/java/com/dsalgoportal/pages/SignInPage.java
@@ -40,7 +40,7 @@ public class SignInPage extends BasePage {
 
 	public void clickLoginBtn() {
 		clickBtn.click();
-		System.out.println();
+		//System.out.println();
 	}
 
 	// login for excel sheet data
