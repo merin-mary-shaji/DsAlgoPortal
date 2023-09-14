9d6c7b981203ec19cc437c73b3b72cec6962bcc9
merin-mary-shaji <142540160+merin-mary-shaji@users.noreply.github.com>
2023-09-13 21:44:46 -0500

    Update TestNGTestRunner.java

--- src/test/java/com/dsalgoportal/testrunner/TestNGTestRunner.java
+++ src/test/java/com/dsalgoportal/testrunner/TestNGTestRunner.java
@@ -1,13 +1,13 @@
 package com.dsalgoportal.testrunner;
 
-import org.junit.runner.RunWith;
+
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
 
-@CucumberOptions(features = "src/test/resources/Features/DSIntro.feature", glue = "com.dsalgoportal.stepdefintion", monochrome = true, dryRun = false, plugin = {
+@CucumberOptions(features = "src/test/resources/Features", glue = "com.dsalgoportal.stepdefintion", monochrome = true, dryRun = false, plugin = {
 		"html:target/cucumber.html" })
 
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
