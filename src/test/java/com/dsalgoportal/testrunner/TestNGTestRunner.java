9c4a26a88eb94faf879a2f50124aaf2c97ff4fc0
merin <anish@Anish-PC.lan>
2023-09-14 00:04:01 -0500

    feature files

--- src/test/java/com/dsalgoportal/testrunner/TestNGTestRunner.java
+++ src/test/java/com/dsalgoportal/testrunner/TestNGTestRunner.java
@@ -7,8 +7,10 @@ import io.cucumber.testng.CucumberOptions;
 
 
 
-@CucumberOptions(features = "src/test/resources/Features", glue = "com.dsalgoportal.stepdefintion", monochrome = true, dryRun = false, plugin = {
-		"html:target/cucumber.html" })
+@CucumberOptions(features = "src/test/resources/Features",
+  glue = "com.dsalgoportal.stepdefintion", 
+   monochrome = true, dryRun = false, 
+   plugin = {"html:target/cucumber.html" })
 
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
 
